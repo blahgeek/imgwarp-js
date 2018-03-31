d8710422b1ac2fc525629fa3bd1f8049897a37ba
BlahGeek <i@BlahGeek.com>
2018-03-31 10:44:37 +0800

    add export/import, add ratio slider

--- js/main.js
+++ js/main.js
@@ -2,79 +2,92 @@ var ImgWarper = ImgWarper || {};
 
 
 $(document).ready(function(){  
-  var canvas = $("#main-canvas")[0];
-  var warper = null;
+    var canvas = $("#main-canvas")[0];
+    var warper = null;
 
-  var holder = document.getElementById('drop-area');
+    var config_text = $('#config-text');
+    var config_apply_btn = $('#config-apply-btn');
 
-  $('.allow-drag').each(function (ele) {
-    this.ondragstart = function (e) {
-      e.dataTransfer.setData('text', this.src);
-      console.log(e.dataTransfer.getData('text'));
-      console.log(e);
-    };
-  });
+    var holder = document.getElementById('drop-area');
 
-  $('.redraw').change(function () {
-    if (warper) {
-      warper.redraw();
-    }
-  });
+    $('.allow-drag').each(function (ele) {
+        this.ondragstart = function (e) {
+            e.dataTransfer.setData('text', this.src);
+            console.log(e.dataTransfer.getData('text'));
+            console.log(e);
+        };
+    });
 
-  holder.ondragover = function () { this.className = 'hover'; return false; };
-  holder.ondragend = function () { this.className = ''; return false; };
-  holder.ondrop = function (e) {
-    this.className = '';
-    e.stopPropagation();
-    e.preventDefault();
-
-    if (e.dataTransfer.files.length > 0) {
-      var file = e.dataTransfer.files[0];
-      var reader = new FileReader();
-      // Prevent any non-image file type from being read.
-      if(!file.type.match(/image.*/)){
-          console.log("The dropped file is not an image: ", file.type);
-          return;
-      }
-      reader.onload = function (event) {
-        console.log(event.target);
-        var img = render(event.target.result, function (imageData) {
-          if (warper) {
-            delete warper;
-          }
-          warper = new ImgWarper.PointDefiner(canvas, img, imageData);
-        });
-      };
-      reader.readAsDataURL(file);
-    } else {
-      var src = e.dataTransfer.getData('text');
-      var img = render(src, function (imageData) {
+    $('.redraw').on("input change", function () {
         if (warper) {
-          delete warper;
+            warper.redraw();
         }
-        warper = new ImgWarper.PointDefiner(canvas, img, imageData);
-      });
-    }
-    return false;
-  };
+    });
+
+    config_apply_btn.click(function() {
+        if (warper) {
+            warper.import(config_text.val());
+        }
+    });
+
+    var on_redraw = function() {
+        config_text.val(warper.export());
+    };
+
+    holder.ondragover = function () { this.className = 'hover'; return false; };
+    holder.ondragend = function () { this.className = ''; return false; };
+    holder.ondrop = function (e) {
+        this.className = '';
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length > 0) {
+            var file = e.dataTransfer.files[0];
+            var reader = new FileReader();
+            // Prevent any non-image file type from being read.
+            if(!file.type.match(/image.*/)){
+                console.log("The dropped file is not an image: ", file.type);
+                return;
+            }
+            reader.onload = function (event) {
+                console.log(event.target);
+                var img = render(event.target.result, function (imageData) {
+                    if (warper) {
+                        delete warper;
+                    }
+                    warper = new ImgWarper.PointDefiner(canvas, img, imageData, on_redraw);
+                });
+            };
+            reader.readAsDataURL(file);
+        } else {
+            var src = e.dataTransfer.getData('text');
+            var img = render(src, function (imageData) {
+                if (warper) {
+                    delete warper;
+                }
+                warper = new ImgWarper.PointDefiner(canvas, img, imageData, on_redraw);
+            });
+        }
+        return false;
+    };
 });
 
 var MAX_HEIGHT = 500;
 function render(src, callback){
-  var image = new Image();
-  image.onload = function(){
-    var canvas = document.getElementById("myCanvas");
-    if(image.height > MAX_HEIGHT) {
-      image.width *= MAX_HEIGHT / image.height;
-      image.height = MAX_HEIGHT;
-    }
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = image.width;
-    canvas.height = image.height;
-    ctx.drawImage(image, 0, 0, image.width, image.height);
-    callback(ctx.getImageData(0, 0, image.width, image.height));
-  };
-  image.src = src;
-  return image;
+    var image = new Image();
+    image.onload = function(){
+        var canvas = document.getElementById("myCanvas");
+        if(image.height > MAX_HEIGHT) {
+            image.width *= MAX_HEIGHT / image.height;
+            image.height = MAX_HEIGHT;
+        }
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+        callback(ctx.getImageData(0, 0, image.width, image.height));
+    };
+    image.src = src;
+    return image;
 }
--- js/point_definer.js
+++ js/point_definer.js
@@ -1,124 +1,177 @@
 var ImgWarper = ImgWarper || {};
 
-ImgWarper.PointDefiner = function(canvas, image, imgData) {
-  this.oriPoints = new Array();
-  this.dstPoints = new Array();
+ImgWarper.PointDefiner = function(canvas, image, imgData, onRedraw) {
+    this.oriPoints = new Array();
+    this.dstPoints = new Array();
 
-  //set up points for change; 
-  var c = canvas;
-  this.canvas = canvas;
-  var that = this;
-  this.dragging_ = false;
-  this.computing_ = false;
-  $(c).unbind();
-  $(c).bind('mousedown', function (e) { that.touchStart(e); });
-  $(c).bind('mousemove', function (e) { that.touchDrag(e); });
-  $(c).bind('mouseup', function (e) { that.touchEnd(e); });
-  this.currentPointIndex = -1;
-  this.imgWarper = new ImgWarper.Warper(c, image, imgData);
+    //set up points for change; 
+    var c = canvas;
+    this.canvas = canvas;
+    var that = this;
+    this.dragging_ = false;
+    this.computing_ = false;
+    $(c).unbind();
+    $(c).bind('mousedown', function (e) { that.touchStart(e); });
+    $(c).bind('mousemove', function (e) { that.touchDrag(e); });
+    $(c).bind('mouseup', function (e) { that.touchEnd(e); });
+    this.currentPointIndex = -1;
+    this.onRedraw = onRedraw;
+    this.imgWarper = new ImgWarper.Warper(c, image, imgData);
 };
 
 ImgWarper.PointDefiner.prototype.touchEnd = function(event) {
-  this.dragging_ = false;
+    this.dragging_ = false;
 }
 
 ImgWarper.PointDefiner.prototype.touchDrag = function(e) {
-  if (this.computing_ || !this.dragging_ || this.currentPointIndex < 0) {
-    return;
-  }
-  this.computing_ = true;
-  e.preventDefault();
-  var endX = (e.offsetX || e.clientX - $(e.target).offset().left);
-  var endY = (e.offsetY || e.clientY - $(e.target).offset().top);
+    if (this.computing_ || !this.dragging_ || this.currentPointIndex < 0) {
+        return;
+    }
+    this.computing_ = true;
+    e.preventDefault();
+    var endX = (e.offsetX || e.clientX - $(e.target).offset().left);
+    var endY = (e.offsetY || e.clientY - $(e.target).offset().top);
 
-  movedPoint = new ImgWarper.Point(endX, endY);
-  this.dstPoints[this.currentPointIndex] = new ImgWarper.Point(endX, endY);
-  this.redraw();
-  this.computing_ = false;
+    movedPoint = new ImgWarper.Point(endX, endY);
+    this.dstPoints[this.currentPointIndex] = new ImgWarper.Point(endX, endY);
+    this.redraw();
+    this.computing_ = false;
 };
 
 ImgWarper.PointDefiner.prototype.redraw = function () {
-  if (this.oriPoints.length < 3) {
-    if (document.getElementById('show-control').checked) {
-      this.redrawCanvas();
+    if (this.onRedraw)
+        this.onRedraw();
+
+    if (this.oriPoints.length < 3) {
+        if (document.getElementById('show-control').checked) {
+            this.redrawCanvas();
+        }
+        return;
     }
-    return;
-  }
-  this.imgWarper.warp(this.oriPoints, this.dstPoints);
-  if (document.getElementById('show-control').checked) {
-    this.redrawCanvas();
-  }
+
+    var morph_ratio = parseFloat(document.getElementById('morph-ratio').value);
+    if (!(morph_ratio >= 0 && morph_ratio <= 1))
+        morph_ratio = 1.0;
+    var dstpoints_ratio = new Array();
+    for (var i = 0 ; i < this.dstPoints.length ; i += 1) {
+        var delta = this.dstPoints[i].subtract(this.oriPoints[i]);
+        delta.x *= morph_ratio;
+        delta.y *= morph_ratio;
+        dstpoints_ratio.push(delta.add(this.oriPoints[i]));
+    }
+    this.imgWarper.warp(this.oriPoints, dstpoints_ratio);
+
+    if (document.getElementById('show-control').checked) {
+        this.redrawCanvas();
+    }
 };
+
+ImgWarper.PointDefiner.prototype.import = function(s) {
+    var points = null;
+    try {
+        points = JSON.parse(s);
+    } catch (e) {
+        return;
+    }
+
+    if (!points)
+        return;
+
+    this.oriPoints = new Array();
+    this.dstPoints = new Array();
+
+    for (var i = 0 ; i < points.length ; i += 1) {
+        this.oriPoints.push(new ImgWarper.Point(points[i].src_x, points[i].src_y));
+        this.dstPoints.push(new ImgWarper.Point(points[i].dst_x, points[i].dst_y));
+    }
+    this.currentPointIndex = -1;
+
+    this.redraw();
+}
+
+ImgWarper.PointDefiner.prototype.export = function() {
+    var points = [];
+    for (var i = 0 ; i < this.oriPoints.length ; i += 1) {
+        points.push({
+            id: i,
+            src_x: this.oriPoints[i].x,
+            src_y: this.oriPoints[i].y,
+            dst_x: this.dstPoints[i].x,
+            dst_y: this.dstPoints[i].y,
+        });
+    }
+    return JSON.stringify(points).replace('},', '},\n');
+}
 
 
 ImgWarper.PointDefiner.prototype.touchStart = function(e) {
-  this.dragging_ = true;
-  e.preventDefault();
-  var startX = (e.offsetX || e.clientX - $(e.target).offset().left);
-  var startY = (e.offsetY || e.clientY - $(e.target).offset().top);
-  var q = new ImgWarper.Point(startX, startY);
+    this.dragging_ = true;
+    e.preventDefault();
+    var startX = (e.offsetX || e.clientX - $(e.target).offset().left);
+    var startY = (e.offsetY || e.clientY - $(e.target).offset().top);
+    var q = new ImgWarper.Point(startX, startY);
 
-  if (e.ctrlKey) {
-    this.oriPoints.push(q);
-    this.dstPoints.push(q);
-  } else if (e.shiftKey) {
-    var pointIndex = this.getCurrentPointIndex(q);  
-    if (pointIndex >= 0) {
-      this.oriPoints.splice(pointIndex, 1);
-      this.dstPoints.splice(pointIndex, 1);
+    if (e.ctrlKey) {
+        this.oriPoints.push(q);
+        this.dstPoints.push(q);
+    } else if (e.shiftKey) {
+        var pointIndex = this.getCurrentPointIndex(q);  
+        if (pointIndex >= 0) {
+            this.oriPoints.splice(pointIndex, 1);
+            this.dstPoints.splice(pointIndex, 1);
+        }
+    } else {
+        this.currentPointIndex = this.getCurrentPointIndex(q);  
     }
-  } else {
-    this.currentPointIndex = this.getCurrentPointIndex(q);  
-  }
-  this.redraw();
+    this.redraw();
 };
 
 ImgWarper.PointDefiner.prototype.getCurrentPointIndex = function(q) {
-  var currentPoint = -1;   
+    var currentPoint = -1;   
 
-  for (var i = 0 ; i< this.dstPoints.length; i++){
-    if (this.dstPoints[i].InfintyNormDistanceTo(q) <= 20) {
-      currentPoint = i;
-      return i;
+    for (var i = 0 ; i< this.dstPoints.length; i++){
+        if (this.dstPoints[i].InfintyNormDistanceTo(q) <= 20) {
+            currentPoint = i;
+            return i;
+        }
     }
-  }
-  return currentPoint;
+    return currentPoint;
 };
 
 ImgWarper.PointDefiner.prototype.redrawCanvas = function(points) {
-  var ctx = this.canvas.getContext("2d");
-  for (var i = 0; i < this.oriPoints.length; i++){
-    if (i < this.dstPoints.length) {
-      if (i == this.currentPointIndex) {
-        this.drawOnePoint(this.dstPoints[i], ctx, 'orange');
-      } else {
-        this.drawOnePoint(this.dstPoints[i], ctx, '#6373CF');
-      }
+    var ctx = this.canvas.getContext("2d");
+    for (var i = 0; i < this.oriPoints.length; i++){
+        if (i < this.dstPoints.length) {
+            if (i == this.currentPointIndex) {
+                this.drawOnePoint(this.dstPoints[i], ctx, 'orange');
+            } else {
+                this.drawOnePoint(this.dstPoints[i], ctx, '#6373CF');
+            }
 
-      ctx.beginPath();
-      ctx.lineWidth = 3;
-      ctx.moveTo(this.oriPoints[i].x, this.oriPoints[i].y);
-      ctx.lineTo(this.dstPoints[i].x, this.dstPoints[i].y);
-      //ctx.strokeStyle = '#691C50';
-      ctx.stroke();
-    } else {
-      this.drawOnePoint(this.oriPoints[i], ctx, '#119a21');
-    }
-  } 
-  ctx.stroke();
+            ctx.beginPath();
+            ctx.lineWidth = 3;
+            ctx.moveTo(this.oriPoints[i].x, this.oriPoints[i].y);
+            ctx.lineTo(this.dstPoints[i].x, this.dstPoints[i].y);
+            //ctx.strokeStyle = '#691C50';
+            ctx.stroke();
+        } else {
+            this.drawOnePoint(this.oriPoints[i], ctx, '#119a21');
+        }
+    } 
+    ctx.stroke();
 };
 
 ImgWarper.PointDefiner.prototype.drawOnePoint = function(point, ctx, color) {
-  var radius = 10; 
-  ctx.beginPath();
-  ctx.lineWidth = 3;
-  ctx.arc(parseInt(point.x), parseInt(point.y), radius, 0, 2 * Math.PI, false);
-  ctx.strokeStyle = color;
-  ctx.stroke();
+    var radius = 10; 
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.arc(parseInt(point.x), parseInt(point.y), radius, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = color;
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.lineWidth = 1;
-  ctx.arc(parseInt(point.x), parseInt(point.y), 3, 0, 2 * Math.PI, false);
-  ctx.fillStyle = color;
-  ctx.fill(); 
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.arc(parseInt(point.x), parseInt(point.y), 3, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    ctx.fill(); 
 };
